3f497443bed6e07c545769d7142bc8b3e577e2e0
tliner-friaca-dev <tlinerfc@hotmail.com>
2023-06-13 17:55:12 -0300

    first commit

---
+++ src/main/java/br/com/Java16Features/Java16FeaturesApplication.java
@@ -0,0 +1,18 @@
+package br.com.Java16Features;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java16FeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java16FeaturesApplication.class, args);
+
+		System.out.println("\n----------------------");
+		System.out.println("ENTROU NA APLICAÇÃO!!!");
+		System.out.println("----------------------\n");
+		
+	}
+
+}
---
+++ src/test/java/br/com/Java16Features/Java16FeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Java16Features;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java16FeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
